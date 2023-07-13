5c150f98047cb125a813629542a7a96d032273e6
rajashekarreddyk <rajshekar429@gmail.com>
2023-07-13 17:05:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,21 @@
 
+import random
+
+def guess_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Guess a number between 1 and 100: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low. Try again!")
+        elif guess > secret_number:
+            print("Too high. Try again!")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+if __name__ == '__main__':
+    guess_number()
